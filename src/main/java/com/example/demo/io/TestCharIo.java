878f6790f1a78444a8db7545ac2a74121d994788
cuizhixiang <15953185712@163.com>
2017-07-26 16:51:29 +0800

    CharArrayReader与CharArrayWriter

--- src/main/java/com/example/demo/io/TestCharIo.java
+++ src/main/java/com/example/demo/io/TestCharIo.java
@@ -7,16 +7,17 @@ import java.io.CharArrayWriter;
  * Created by cuizhixiang on 2017/7/4.
  **/
 public class TestCharIo {
-    public static void main(String args[])throws Exception{
+    public static void main(String args[]) throws Exception {
         test();
     }
-    public static void test()throws Exception{
+
+    public static void test() throws Exception {
         CharArrayReader charArrayReader = new CharArrayReader("fdsjfkdsjkl".toCharArray());
         CharArrayWriter charArrayWriter = new CharArrayWriter();
         char[] c = new char[1024];
         int len;
-        while((len = charArrayReader.read(c)) != -1){
-            charArrayWriter.write(c,0,len);
+        while ((len = charArrayReader.read(c)) != -1) {
+            charArrayWriter.write(c, 0, len);
         }
         System.out.println(charArrayWriter.toString());
         System.out.println(charArrayWriter.toCharArray());
