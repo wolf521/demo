63302f3b677cdf5a7ddc371f1be943981c8545ef
cuizhixiang <15953185712@163.com>
2017-10-31 17:33:04 +0800

    文件压缩及解压缩

---
+++ src/main/java/com/example/demo/connect/MyConnect.java
@@ -0,0 +1,16 @@
+package com.example.demo.connect;
+
+/**
+ * Created by Administrator on 2017/10/30.
+ */
+public class MyConnect {
+    public static void main(String agrs[]){
+        int[] arr = {1,5,9,3,8,7,2};
+        int[] index = {0,1,2,1,3,0,4,1,5,0,6};
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i = 0;i < 11;i++){
+            stringBuffer.append(arr[index[i]]);
+        }
+        System.out.println(stringBuffer.toString());
+    }
+}
