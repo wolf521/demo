8ed2758db95f11f4746ce735be3f53a7a52f902b
cuizhixiang <15953185712@163.com>
2018-01-18 13:43:04 +0800

    注解

--- src/main/java/com/example/demo/annotation/controller/AnnotionController.java
+++ src/main/java/com/example/demo/annotation/controller/AnnotionController.java
@@ -8,5 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AnnotionController{
 
-
+/**
+ * 元数据：又称中介数据、中继数据，为描述数据的数据，主要是描述数据属性的信息，用来支持如指示存储位置、历史数据、资源查找、文件记录等功能。
+ * java注解概念：Java提供了一种原程序中的元素关联任何信息和任何元数据的途径和方法
+ * Java中常见注解：
+ *      1：JDK自带注解：@Override：覆盖标志、@Deprecated：设置方法过时、@Suppvisewaraing：忽略某警告
+ */
 }
--- src/main/java/com/example/demo/connect/MyConnect.java
+++ src/main/java/com/example/demo/connect/MyConnect.java
@@ -6,12 +6,6 @@ package com.example.demo.connect;
  */
 public class MyConnect {
     public static void main(String agrs[]) {
-        int[] arr = {1, 5, 9, 3, 8, 7, 2};
-        int[] index = {0, 1, 2, 1, 3, 0, 4, 1, 5, 0, 6};
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; i < 11; i++) {
-            stringBuffer.append(arr[index[i]]);
-        }
-        System.out.println(stringBuffer.toString());
+        System.out.println("");
     }
 }
